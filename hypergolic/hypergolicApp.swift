854fa0a146536094e7ecc2b1d81c99b06217fcfc
Saul Nunez <saul.nunez99@gmail.com>
2025-06-09 14:40:30 -0600

    Initial Commit

---
+++ hypergolic/hypergolicApp.swift
@@ -0,0 +1,17 @@
+//
+//  hypergolicApp.swift
+//  hypergolic
+//
+//  Created by Saúl Núñez Castruita on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct hypergolicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hypergolicTests/hypergolicTests.swift
@@ -0,0 +1,16 @@
+//
+//  hypergolicTests.swift
+//  hypergolicTests
+//
+//  Created by Saúl Núñez Castruita on 09/06/25.
+//
+
+import Testing
+
+struct hypergolicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
