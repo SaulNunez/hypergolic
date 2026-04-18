25b2288a6cf249355b237d95d92dcb4199576436
Saul Nunez <saul.nunez99@gmail.com>
2026-04-18 09:39:40 -0600

    Add struct for providing ready to download images to users

---
+++ hypergolic/models/OSImage.swift
@@ -0,0 +1,15 @@
+//
+//  OSImage.swift
+//  hypergolic
+//
+//  Created by Saul Nunez Castruita on 18/04/26.
+//
+
+import Foundation
+
+
+struct OSImage {
+    var url: String
+    var icon: String
+    var humanRedableName: String
+}
